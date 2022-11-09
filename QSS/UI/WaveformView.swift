ce6617f5e565d9f764e59c65c5318c0399957343
Denis Beloshitskiy <beloshitskiy.denis@gmail.com>
2022-11-10 02:54:41 +0300

    Refactor WaveformView and add Steppers

--- QSS/UI/WaveformView.swift
+++ QSS/UI/WaveformView.swift
@@ -33,18 +33,51 @@ struct WaveformView: View {
   @EnvironmentObject var appState: AppState
   var body: some View {
     VStack {
-      Chart(appState.simulation.chartData) {
-        LineMark($0.coordinates)
-          .foregroundStyle(by: .value("Actor", $0.actor))
+      Chart {
+        ForEach(appState.simulation.generators) { gen in
+          ForEach(gen.chartData) {
+            LineMark($0, str: num(gen))
+              .foregroundStyle(by: .value("Actor", "Generators"))
+          }
+        }
+        ForEach(appState.simulation.handlers) { han in
+          ForEach(han.chartData) {
+            LineMark($0, str: num(han))
+              .foregroundStyle(by: .value("Actor", "Handlers"))
+          }
+        }
+        ForEach(appState.simulation.buffers) { buf in
+          ForEach(buf.chartData) {
+            LineMark($0, str: num(buf))
+              .foregroundStyle(by: .value("Actor", "Buffers"))
+          }
+        }
+        ForEach(appState.simulation.rejector.chartData) {
+          LineMark($0, str: "Rejector")
+            .foregroundStyle(by: .value("Actor", "Rejector"))
+        }
       }
       ControlView()
     }
+    .padding()
     .frame(minWidth: 500,
            idealWidth: 750,
            maxWidth: .infinity,
            minHeight: 500,
            idealHeight: 600,
            maxHeight: .infinity)
+  }
+
+  private func num(_ actor: any WaveformConvertible) -> String {
+    switch actor {
+    case is Generator:
+      return "Generator\(String(describing: appState.simulation.generators.firstIndex { $0 === actor as! Generator }))"
+    case is Handler:
+      return "Handler\(String(describing: appState.simulation.handlers.firstIndex { $0 === actor as! Handler }))"
+    case is Buffer:
+      return "Buffer\(String(describing: appState.simulation.buffers.firstIndex { $0 === actor as! Buffer }))"
+    default: return ""
+    }
   }
 }
 
@@ -58,5 +91,17 @@ struct WaveformView_Previews: PreviewProvider {
 extension LineMark {
   init(_ point: Point) {
     self.init(x: .value("x", point.x), y: .value("y", point.y))
+  }
+
+  init(_ wPoint: WaveformPoint) {
+    self.init(x: .value("x", wPoint.x),
+              y: .value("y", wPoint.y),
+              series: .value("Actor", wPoint.actor))
+  }
+
+  init(_ wPoint: WaveformPoint, str: String) {
+    self.init(x: .value("x", wPoint.x),
+              y: .value("y", wPoint.y),
+              series: .value("Actor", str))
   }
 }
